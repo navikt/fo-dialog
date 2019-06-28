3da784cd4dada428f990ac52a5022ec79cb86804
ingeborg.sollid <ingsollid@gmail.com>
2019-06-28 08:27:43 +0200

    Fjern brodsmuler og legg til ikon

--- src/view/DialogBanner.tsx
+++ src/view/DialogBanner.tsx
@@ -1,29 +1,16 @@
 import React from "react"
-import PersonIcon from "./PersonIcon";
-import Lenke from 'nav-frontend-lenker';
 import { Sidetittel } from "nav-frontend-typografi";
+import { ReactComponent as DialogIcon} from "./dialog.svg";
 
 import './DialogBanner.less'
 
 export function DialogBanner () {
     return (
-        <div className="dialogBanner">
-            <div className="linkPathContainer">
-                <div className="PersonIcon">
-                    <PersonIcon/>
-                </div>
-                <nav >
-                    <ol className="linkPathList">
-                        <li>
-                            <Lenke className="linkPathList-item" href="#">Ditt nav</Lenke>
-                        </li>
-                        <li>
-                            <Lenke className="linkPathList-item" href="#">Veien til arbeid</Lenke>
-                        </li>
-                    </ol>
-                </nav>
+        <div className="dialogbanner">
+            <div className="sidetittel-container">
+                <DialogIcon className="dialog-ikon"/>
+                <Sidetittel className="dialog-sidetittel">Dialog med din veileder</Sidetittel>
             </div>
-            <Sidetittel className="dialogSideTittel">Dialog med din veileder</Sidetittel>
         </div>
     )
 }
