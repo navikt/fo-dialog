a2717471fa696641d19a802337a4cf2a6f4ac394
Henrik Fauskanger <henrik.fauskanger@gmail.com>
2019-06-28 12:49:38 +0200

    Formater datostreng som i henvendelser

--- src/view/DialogPreview.tsx
+++ src/view/DialogPreview.tsx
@@ -7,20 +7,23 @@ import {LenkepanelBase} from "nav-frontend-lenkepanel";
 
 import {EtikettListe} from "./EtikettListe";
 import "./styles.less";
+import {formaterHenvendelseDate} from "../utils/functions";
+import {string} from "prop-types";
 
 interface Props {
     dialog: DialogData;
 }
 
 
-export function DialogPreview(props: Props){
+export function DialogPreview(props: Props) {
 
+    const datoString: string = (typeof props.dialog.sisteDato === "string") ? formaterHenvendelseDate(props.dialog.sisteDato) : "";
 
     return (
-        <LenkepanelBase className="dialog-preview" >
+        <LenkepanelBase className="dialog-preview">
 
             <div className="dialog-preview__internal-div">
-                <EtikettLiten>{props.dialog.sisteDato}</EtikettLiten>
+                <EtikettLiten>{datoString}</EtikettLiten>
                 <Systemtittel className="lenkepanel__heading"> {props.dialog.overskrift}</Systemtittel>
                 <Normaltekst className="dialog-preview__last-henvendelse">{props.dialog.sisteTekst}</Normaltekst>
                 <EtikettListe dialog={props.dialog}/>
