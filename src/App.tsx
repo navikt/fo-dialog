1f790ab4f7c7849888dae5bd1820c042fc0419b2
ingeborg.sollid <ingsollid@gmail.com>
2019-07-03 10:43:01 +0200

    Flytt ut fetch-kall ut av App.tsx til felles komponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,33 @@
 import React, { useEffect, useState} from 'react';
 import './App.less';
 import {fetchData} from "./utils/fetch";
-import {Bruker, DialogData} from "./utils/typer";
+import { DialogData } from "./utils/typer";
 import {HenvendelseList} from "./view/HenvendelseList";
-import { UserInfoContext, OppfolgingContext} from "./Context";
+import { Provider} from "./Context";
 import {DialogBanner} from "./view/DialogBanner";
 import {DialogOverview} from "./view/DialogOverview";
-import oppfolgingData from "./mock/oppfolging";
 import {AlertStripeContainer} from "./view/AlertStripeContainer";
 
 import './App.less';
-
-
 
 
 const App = () => {
 
     const [dialogListe, setDialogListe] = useState<DialogData[] | undefined>(undefined);
 
-    const [userInfo, setUserInfo] = useState<Bruker | undefined>(undefined);
-
-
     useEffect(() => {
         fetchData<DialogData[]>("/veilarbdialog/api/dialog", {method: 'get'})
             .then(res => setDialogListe(res));
-        fetchData<Bruker>("/veilarboppfolging/api/oppfolging/me", {method: 'get'})
-            .then(res => setUserInfo(res))
     }, []);
-
     return (<>
             <div className="app">
                 <DialogBanner/>
-                <OppfolgingContext.Provider value={oppfolgingData}>
-                    <UserInfoContext.Provider value={userInfo}>
-                        <AlertStripeContainer/>
-                        {dialogListe === undefined ? null : <DialogOverview dialogData={dialogListe} visible={oppfolgingData.underOppfolging}/>}
-                        <div className="henvendelseList"> {dialogListe === undefined ? null :
-                            <HenvendelseList henvendelseDataList={dialogListe[0].henvendelser}/>}</div>
-                    </UserInfoContext.Provider>
-                </OppfolgingContext.Provider>
+                <Provider>
+                    <AlertStripeContainer/>
+                    {dialogListe === undefined ? null : <DialogOverview dialogData={dialogListe} visible={false}/>}
+                    <div className="henvendelseList"> {dialogListe === undefined ? null :
+                        <HenvendelseList henvendelseDataList={dialogListe[0].henvendelser}/>}</div>
+                </Provider>
             </div>
         </>
     );
