3f16b89ab8e4813d74e5c1b9b08ab018c181d2d8
ingeborg.sollid <ingsollid@gmail.com>
2019-07-04 10:05:37 +0200

    Bytt til conditional rendering i AlertStripeContainer

--- src/view/AlertStripeContainer.tsx
+++ src/view/AlertStripeContainer.tsx
@@ -1,13 +1,9 @@
 import React, {useContext} from "react";
 import {AlertStripeAdvarsel} from "nav-frontend-alertstriper";
-import {visibleIfHoc} from "../component/hoc/visibleIfHoc";
 import {OppfolgingContext, UserInfoContext} from "../Context";
 import {Normaltekst} from "nav-frontend-typografi";
 
 import "./alertstripe-container.less"
-
-const OppfolgingAlertStripeVisibleProp  = visibleIfHoc(AlertStripeAdvarsel);
-
 
 export function AlertStripeContainer(){
     const oppfolgingData = useContext(OppfolgingContext);
@@ -16,29 +12,23 @@ export function AlertStripeContainer(){
     const erVeileder = UserInfo!.erVeileder;
     const erUnderOppfolging= oppfolgingData!.underOppfolging;
     const harOppfolgingsPerioder = oppfolgingData!.oppfolgingsPerioder.length > 0;
+
+    if (!erUnderOppfolging && !harOppfolgingsPerioder){
+        if (erVeileder){
+            return (<AlertStripeAdvarsel className="oppfolging-alertstripe">Denne brukeren er ikke registrert.</AlertStripeAdvarsel>)
+        }
+        else{
+            return(<AlertStripeAdvarsel className="oppfolging-alertstripe">Du må være registrert hos NAV for å bruke aktivitetsplanen.</AlertStripeAdvarsel>)
+        }
+    }
+    if (!erUnderOppfolging && harOppfolgingsPerioder && !erVeileder )
     return (
-        <div>
-            <div className="oppfolging-alertstripe">
-                <OppfolgingAlertStripeVisibleProp visible={(!erVeileder && !erUnderOppfolging && !harOppfolgingsPerioder)}>
-                    Du må være registrert hos NAV for å bruke aktivitetsplanen.
-                </OppfolgingAlertStripeVisibleProp>
-            </div>
-            <div className="oppfolging-alertstripe">
-                <OppfolgingAlertStripeVisibleProp visible={!erVeileder && !erUnderOppfolging && harOppfolgingsPerioder}>
-                    <Normaltekst>
-                        Du er ikke lenger registrert hos NAV og din tidligere aktivitetsplan er lagt under "Mine tidligere planer". Hvis du fortsatt skal motta ytelser, få oppfølging fra NAV og bruke aktivitetsplanen må du være registrert.
-                    </Normaltekst>
-                     <a href="https://www.nav.no/Forsiden">www.nav.no</a>
-                </OppfolgingAlertStripeVisibleProp>
-            </div>
-            <div className="oppfolging-alertstripe">
-                <OppfolgingAlertStripeVisibleProp visible={erVeileder && !erUnderOppfolging && !harOppfolgingsPerioder}>
-                    Denne brukeren er ikke registrert.
-                </OppfolgingAlertStripeVisibleProp>
-            </div>
-
-        </div>
-    )
-
-
+            <AlertStripeAdvarsel>
+                <Normaltekst>
+                Du er ikke lenger registrert hos NAV og din tidligere aktivitetsplan er lagt under "Mine tidligere planer". Hvis du fortsatt skal motta ytelser, få oppfølging fra NAV og bruke aktivitetsplanen må du være registrert.
+                </Normaltekst>
+                <a href="https://www.nav.no/Forsiden">www.nav.no</a>
+            </AlertStripeAdvarsel>
+    );
+    return <></>
 }
