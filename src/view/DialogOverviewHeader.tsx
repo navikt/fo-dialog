8a26f67ca2f3a4a9012f194f4e43a8d487c48c14
Henrik Fauskanger <henrik.fauskanger@gmail.com>
2019-07-04 09:36:58 +0200

    Sett synlighet til viktige meldinger-checkbox av når bruker ikke har noen viktige dialoger

--- src/view/DialogOverviewHeader.tsx
+++ src/view/DialogOverviewHeader.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 import {Knapp} from "nav-frontend-knapper";
-import {Checkbox} from "nav-frontend-skjema";
+import {DialogData} from "../utils/typer";
+import {Checkbox} from "../component/checkbox/Checkbox";
 
+interface Props {
+    dialogData: DialogData[];
+}
 
+export function DialogOverviewHeader(props: Props) {
 
+    const brukerHarViktigeDialoger = !!props.dialogData.filter((dialog) => dialog.egenskaper.length > 0).length;
 
-export function DialogOverviewHeader(){
 
     return (<div className="dialog-overview__header">
         <Knapp>Ny dialog</Knapp>
-        <Checkbox label={"Viktige Meldinger"}/>
+        <Checkbox label={"Viktige Meldinger"} visible={brukerHarViktigeDialoger}/>
     </div>)
 }
