5a069484d39deb98569d099d0b9d0f31a69c2831
ingeborg.sollid <ingsollid@gmail.com>
2019-07-05 12:26:26 +0200

    Dekomponer Dialog til flere komponenter

--- src/view/Dialog.tsx
+++ src/view/Dialog.tsx
@@ -1,11 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import {DialogData} from "../utils/typer";
-import {Innholdstittel, Undertittel} from "nav-frontend-typografi";
-import Lenke from "nav-frontend-lenker";
-import {HoyreChevron} from "nav-frontend-chevron";
-import {Checkbox, Textarea} from "nav-frontend-skjema";
+import {Innholdstittel} from "nav-frontend-typografi";
 import {HenvendelseList} from "./HenvendelseList";
-import {Hovedknapp} from "nav-frontend-knapper";
+import {DialogCheckboxes} from "./DialogCheckboxes";
+import {DialogSkrivMeld} from "./DialogSkrivMeld";
 
 import './Dialog.less';
 
@@ -14,46 +12,12 @@ interface Props {
 }
 
 export function Dialog(props: Props) {
-    const [value, setValue] =  useState('');
-
     if (props.dialog !== null) {
         return (
             <div className="dialog">
-                <Lenke href="/dialog">
-                    <Undertittel>
-                        Aktivitet: {props.dialog.aktivitetId}
-                        <HoyreChevron/>
-                    </Undertittel>
-                </Lenke>
-                <div className="checkbox-block">
-                    <Checkbox
-                        label='Venter på svar fra NAV'
-                        checked={!props.dialog.venterPaSvar}
-                        className="checkbox-block__item"
-                    />
-                    <Checkbox
-                        label='Venter på svar fra bruker'
-                        checked={props.dialog.venterPaSvar}
-                        className="checkbox-block__item"
-                    />
-                </div>
-
+                <DialogCheckboxes dialog={props.dialog}/>
                 <HenvendelseList henvendelseDataList={props.dialog.henvendelser}/>
-
-                <div className="skriv-melding">
-                    <Textarea
-                        label='Skriv en melding til brukeren'
-                        placeholder="Skriv her .."
-                        textareaClass='meldingsfelt'
-                        id='meldingIn'
-                        name='NyMelding'
-                        onChange={(event) => setValue((event.target as HTMLInputElement).value)}
-                        value={value}
-                    />
-                    <Hovedknapp title="Send"
-                                onClick={() => sendeNyMelding({tekst: value})}
-                    >Send</Hovedknapp>
-                </div>
+                <DialogSkrivMeld/>
             </div>)
     } else {
         return (
@@ -61,10 +25,5 @@ export function Dialog(props: Props) {
                 <Innholdstittel> Ingen Valgt Dialog</Innholdstittel>
             </div>
         )
-
     }
-}
-
-function sendeNyMelding (props: { tekst: string }) {
-    console.log(props);
 }
