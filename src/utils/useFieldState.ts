be488d50151a336f21437ad7c0b569b46d3d0095
Yayla, Satilmis <satilmis.yayla@nav.no>
2019-07-08 17:26:50 +0200

    FO-2372: Legg inn form for ny dialog med feltvise valideringer på hver inntastede bokstav, tab (onBlur), og send (onSubmit)

---
+++ src/utils/useFieldState.ts
@@ -0,0 +1,50 @@
+import React, {useCallback, useState} from "react";
+import {SkjemaelementFeil} from "nav-frontend-skjema/lib/skjemaelement-feilmelding";
+
+export interface FieldState {
+    input: {
+        value: string;
+        onChange: React.ChangeEventHandler;
+        onBlur: React.FocusEventHandler;
+        feil?: SkjemaelementFeil;
+    };
+    validate(): void;
+}
+
+export default function useFieldState(initialState: string, validate: (value: string) => string | null = () => null): FieldState {
+    const [value, setValue] = useState(initialState);
+    const [touched, setTouched] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const onChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const newValue = event.target.value;
+            setValue(newValue);
+            setError(validate(newValue));
+        },
+        [setValue, setError, validate]
+    );
+
+    const onBlur = useCallback(
+        (event: React.FocusEvent<HTMLInputElement>) => {
+            setTouched(true);
+            setError(validate(event.target.value));
+        },
+        [validate, setTouched]
+    );
+
+    const handleValidate = () => {
+        setTouched(true);
+        setError(validate(value));
+    };
+
+    return {
+        input: {
+            value,
+            onChange,
+            onBlur,
+            feil: (!touched || error === null) ? undefined : {feilmelding: error}
+        },
+        validate: handleValidate
+    };
+}
---
+++ src/view/DialogNew.tsx
@@ -0,0 +1,67 @@
+import React, {FormEvent} from "react";
+import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
+import {Input, Textarea} from "nav-frontend-skjema";
+import {Hovedknapp} from "nav-frontend-knapper";
+
+import './Dialog.less';
+import useFieldState from "../utils/useFieldState";
+
+function validerMelding(melding: string): string | null {
+    if (melding.length === 0) {
+        return "Melding må ha innhold.";
+    } else if (melding.length < 10) {
+        return "Meldingen må være lengre enn 10 tegn.";
+    } else {
+        return null;
+    }
+}
+
+export function DialogNew() {
+    const tema = useFieldState('', value => {
+        console.log('tema', value);
+        return value.length === 0 ? 'Tema må ha innhold.' : null;
+    });
+    const melding = useFieldState('', validerMelding);
+
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        tema.validate();
+        melding.validate();
+
+        if (tema.input.feil || melding.input.feil) {
+            console.log('Det skjedde en feil', tema, melding);
+        } else {
+            console.log("Submitted Succesfully", {tema: tema.input.value, melding: melding.input.value});
+        }
+    }
+
+
+    return (<>
+        <form onSubmit={handleSubmit} noValidate>
+            <Innholdstittel>Ny Dialog</Innholdstittel>
+            <Normaltekst>Her kan du skrive til din veileder om arbeid og oppfølging. Du vil få
+                svar i løpet av noen dager.</Normaltekst>
+            <Input
+                label={'Hva er tema for dialogen?'}
+                placeholder="Skriv her"
+                {...tema.input}
+            />
+            <div className="skriv-melding">
+                <Textarea
+                    label='Skriv en melding til brukeren'
+                    placeholder="Skriv her"
+                    {...melding.input}
+                    // TODO Lag PR til nav-frontend som fikser textarea sin onChange. Burde bruke `React.ChangeEvent` fremfor dagens `React.SyntheticEvent`.
+                    onChange={melding.input.onChange as any}
+                />
+                <Hovedknapp
+                    htmlType={"submit"}
+                    title="Send"
+                >
+                    Send
+                </Hovedknapp>
+            </div>
+        </form>
+    </>);
+}
