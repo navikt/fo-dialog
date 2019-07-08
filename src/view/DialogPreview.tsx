b10253b19b9a779e9abf8189b1f70e72755ced11
Henrik Fauskanger <henrik.fauskanger@gmail.com>
2019-07-08 13:48:48 +0200

    Legg til funksjonalitet for åpning av dialoger uten fullstending refresh

--- src/view/DialogPreview.tsx
+++ src/view/DialogPreview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {AnchorHTMLAttributes, HTMLProps} from "react";
 
 import {DialogData} from "../utils/typer";
 
@@ -8,6 +8,8 @@ import {LenkepanelBase} from "nav-frontend-lenkepanel";
 import {EtikettListe} from "./EtikettListe";
 import "./dialogpreview.less";
 import {formaterHenvendelseDate} from "../utils/date";
+import {Link} from "react-router-dom";
+import {classPrivateProperty} from "@babel/types";
 
 interface Props {
     dialog: DialogData;
@@ -19,14 +21,21 @@ export function DialogPreview(props: Props) {
     const datoString = !!props.dialog.sisteDato ? formaterHenvendelseDate(props.dialog.sisteDato) : "";
 
     return (
-        <LenkepanelBase className="dialog-preview" href={`/${props.dialog.id}`}>
+        <>
+            <LenkepanelBase className="dialog-preview" href={`/${props.dialog.id}`} linkCreator={WrapInReactLink}>
 
-            <div className="dialog-preview__internal-div">
-                <EtikettLiten>{datoString}</EtikettLiten>
-                <Systemtittel className="lenkepanel__heading"> {props.dialog.overskrift}</Systemtittel>
-                <Normaltekst className="dialog-preview__last-henvendelse">{props.dialog.sisteTekst}</Normaltekst>
-                <EtikettListe dialog={props.dialog}/>
-            </div>
+                <div className="dialog-preview__internal-div">
+                    <EtikettLiten>{datoString}</EtikettLiten>
+                    <Systemtittel className="lenkepanel__heading"> {props.dialog.overskrift}</Systemtittel>
+                    <Normaltekst className="dialog-preview__last-henvendelse">{props.dialog.sisteTekst}</Normaltekst>
+                    <EtikettListe dialog={props.dialog}/>
+                </div>
 
-        </LenkepanelBase>)
+            </LenkepanelBase>
+        </>)
+}
+
+function WrapInReactLink(props: HTMLProps<HTMLElement>){
+    const innerProps = props as AnchorHTMLAttributes<HTMLAnchorElement>;
+    return <Link to={innerProps.href!} {...innerProps}/>
 }
