e71436bec0b16b5e554f6d05d99d9a19f464227a
ingeborg.sollid <ingsollid@gmail.com>
2019-07-09 14:25:33 +0200

    FO-2371: Endre mockdata

--- src/mock/oppfolging.ts
+++ src/mock/oppfolging.ts
@@ -1,25 +1,29 @@
-const oppfPerioder: any[] = [
+import {OppfolgingData, PeriodeData} from "../utils/typer";
+import {JSONArray} from "yet-another-fetch-mock";
+
+
+const oppfPerioder: PeriodeData[] & JSONArray = [
     {
         aktorId: '1234567988888',
-        veileder: null,
+        veileder: false,
         startDato: '2017-01-30T10:46:10.971+01:00',
         sluttDato: '2017-12-31T10:46:10.971+01:00',
         begrunnelse: null,
     },
     {
         aktorId: '1234567988888',
-        veileder: null,
+        veileder: false,
         startDato: '2018-01-31T10:46:10.971+01:00',
         sluttDato: null,
         begrunnelse: null,
     },
 ];
-const oppfolgingData = {
+const oppfolgingData: OppfolgingData = {
     fnr: null,
     veilederId: "101010",
     reservasjonKRR: false,
     manuell: false,
-    underOppfolging: true, //eller false
+    underOppfolging: false, //eller false
     underKvp: false,
     oppfolgingUtgang: null,
     gjeldendeEskaleringsvarsel: null,
