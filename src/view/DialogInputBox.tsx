00b8fd1c081982ba2303cb0c69ee63038f550afd
Yayla, Satilmis <satilmis.yayla@nav.no>
2019-07-10 16:00:15 +0200

    t til annen branch::."
    This reverts commit 0a129c70d1f2358640b1b6be2e748c6ab606fabd.

--- src/view/DialogInputBox.tsx
+++ src/view/DialogInputBox.tsx
@@ -1,57 +1,31 @@
-import React, {FormEvent} from "react";
+import React, {useState} from "react";
 import {Textarea} from "nav-frontend-skjema";
 import {Hovedknapp} from "nav-frontend-knapper";
-import useFieldState from "../utils/useFieldState";
-import {DialogData} from "../utils/typer";
 
-function validerMelding(melding: string): string | null {
-    if (melding.length === 0) {
-        return "Melding må ha innhold.";
-    } else if (melding.length < 10) {
-        return "Meldingen må være lengre enn 10 tegn.";
-    } else {
-        return null;
-    }
+function sendeNyMelding (props: { tekst: string }) {
+    console.log(props);
 }
 
-interface Props {
-    dialog: DialogData;
-}
+export function DialogInputBox(){
+    const [value, setValue] =  useState('');
 
-export function DialogInputBox(props: Props){
-    const melding = useFieldState('', validerMelding);
-
-    function handleSubmit(event: FormEvent) {
-        event.preventDefault();
-        melding.validate();
-        var dialg : DialogData = props.dialog;
-        if ( melding.input.feil === undefined) {
-            fetch('/veilarbdialog/api/dialog/ny', {
-                method: 'POST', body: JSON.stringify({
-                    tekst: melding.input.value,
-                    dialogId: dialg.id,
-                    overskrift: dialg.overskrift,
-                })
-            });
-        }
-    }
     return (
-        <form onSubmit={handleSubmit} noValidate>
         <div className="skriv-melding" >
             <Textarea
                 label='Skriv en melding til brukeren'
                 placeholder="Skriv her .."
-                {...melding.input}
-                // TODO Lag PR til nav-frontend som fikser textarea sin onChange. Burde bruke `React.ChangeEvent` fremfor dagens `React.SyntheticEvent`.
-                onChange={melding.input.onChange as any}
-            />
+                textareaClass='meldingsfelt'
+                id='meldingIn'
+                name='NyMelding'
+                onChange={(event) => setValue((event.target as HTMLInputElement).value)}
+                value={value} />
             <Hovedknapp
-                htmlType={"submit"}
                 title="Send"
+                onClick={() => sendeNyMelding({tekst: value})}
             >Send
             </Hovedknapp>
         </div>
-        </form>
-
     )
 }
+
+
