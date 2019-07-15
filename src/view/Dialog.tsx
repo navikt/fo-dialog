910df704575fa8654b2efd18f797aca997aedddb
Henrik Fauskanger <henrik.fauskanger@gmail.com>
2019-07-15 09:29:04 +0200

    Test commit please ignore

--- src/view/Dialog.tsx
+++ src/view/Dialog.tsx
@@ -1,45 +1,48 @@
 import React from "react";
-import {DialogData} from "../utils/typer";
+import { DialogData } from "../utils/typer";
 
-import {HenvendelseList} from "./HenvendelseList";
-import {DialogHeader} from "./DialogHeader";
-import {DialogInputBoxVisible} from "./DialogInputBox";
-import {useOppfolgingContext} from "../Context";
-import './Dialog.less';
-import {RouteComponentProps, withRouter} from "react-router";
-import {Aktivitetskort} from "./Aktivitetskort";
-import {AktivitetskortPreview} from "./AktivitetskortPreview";
-import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
+import { HenvendelseList } from "./HenvendelseList";
+import { DialogHeader } from "./DialogHeader";
+import { DialogInputBoxVisible } from "./DialogInputBox";
+import { useOppfolgingContext } from "../Context";
+import "./Dialog.less";
+import { RouteComponentProps, withRouter } from "react-router";
+import { Aktivitetskort } from "./Aktivitetskort";
+import { AktivitetskortPreview } from "./AktivitetskortPreview";
+import { Innholdstittel, Normaltekst } from "nav-frontend-typografi";
 
-interface Props extends RouteComponentProps<{ dialogId?: string; }> {
-    dialogData: DialogData[];
+interface Props extends RouteComponentProps<{ dialogId?: string }> {
+  dialogData: DialogData[];
 }
 
 export function Dialog(props: Props) {
-    const oppfolgingData = useOppfolgingContext();
-    const dialogId = props.match.params.dialogId;
-    const valgtDialog = props.dialogData.find((dialog) => dialog.id === dialogId);
+  const oppfolgingData = useOppfolgingContext();
+  const dialogId = props.match.params.dialogId;
+  const valgtDialog = props.dialogData.find(dialog => dialog.id === dialogId);
 
-    if (!valgtDialog) {
-        return (
-            <div className="dialog">
-                <Innholdstittel>Dialog</Innholdstittel>
-                <Normaltekst>Detaljer for valgt dialog vises her.</Normaltekst>
-            </div>
-        );
-    }
-
+  if (!valgtDialog) {
     return (
-        <>
-            <div className="dialog">
-                <AktivitetskortPreview dialog={valgtDialog}/>
-                <DialogHeader dialog={valgtDialog}/>
-                <HenvendelseList henvendelseDataList={valgtDialog.henvendelser}/>
-                <DialogInputBoxVisible  dialog={valgtDialog} visible={oppfolgingData!.underOppfolging}/>
-            </div>
-            <Aktivitetskort dialog={valgtDialog}/>
-        </>
+      <div className="dialog">
+        <Innholdstittel>Dialog</Innholdstittel>
+        <Normaltekst>Detaler for valgt dialog vises her.</Normaltekst>
+      </div>
     );
+  }
+
+  return (
+    <>
+      <div className="dialog">
+        <AktivitetskortPreview dialog={valgtDialog} />
+        <DialogHeader dialog={valgtDialog} />
+        <HenvendelseList henvendelseDataList={valgtDialog.henvendelser} />
+        <DialogInputBoxVisible
+          dialog={valgtDialog}
+          visible={oppfolgingData!.underOppfolging}
+        />
+      </div>
+      <Aktivitetskort dialog={valgtDialog} />
+    </>
+  );
 }
 
 export default withRouter(Dialog);
