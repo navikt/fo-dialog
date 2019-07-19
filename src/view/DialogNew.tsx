9f8e993f66f096e1a53c41653011d68d9d397969
Yayla, Satilmis <satilmis.yayla@nav.no>
2019-07-19 15:46:51 +0200

    FO-2402: SSP: Vis feilmelding til bruker --uferdig!!-- viser form alert i tillegg til feltvise alerts

--- src/view/DialogNew.tsx
+++ src/view/DialogNew.tsx
@@ -1,5 +1,5 @@
-import React, { FormEvent, useEffect, useState } from 'react';
-import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
+import React, { FormEvent } from 'react';
+import { Innholdstittel, Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import { Input } from 'nav-frontend-skjema';
 import useFieldState from '../utils/useFieldState';
 import { DialogData, NyDialogMeldingData } from '../utils/typer';
@@ -13,60 +13,37 @@ import './Dialog.less';
 
 interface Props extends RouteComponentProps<{}> {}
 
-function validerTema(tema: string): string | null {
-    if (tema.trim().length === 0) {
-        return 'Tema må ha innhold.';
-    } else {
-        return null;
-    }
-}
-function validerMelding(melding: string): string | null {
-    if (melding.trim().length === 0) {
-        return 'Melding må ha innhold.';
-    } else {
-        return null;
-    }
-}
-
-export function useNyDialogFormstatus(): String[] {
-    const [NyDialogFormIsValid, setNyDialogFormValid] = useState();
-    useEffect(() => {
-        function validerNyDialog(nyDialogData: NyDialogMeldingData): String[] {
-            var formErrors: String[] = [];
-            var temaError = nyDialogData.overskrift === undefined ? null : validerTema(nyDialogData.overskrift);
-            var meldingError = validerMelding(nyDialogData.tekst);
-            if (temaError !== null) {
-                formErrors.push(temaError);
-            }
-            if (meldingError !== null) {
-                formErrors.push(meldingError);
-            }
-            if (formErrors.length === 0 && temaError === null && meldingError === null) {
-                setNyDialogFormValid(['']);
-            } else {
-                setNyDialogFormValid(formErrors);
-            }
-            return formErrors;
-        }
-    });
-    return NyDialogFormIsValid;
-}
-
 function DialogNew(props: Props) {
     const tema = useFieldState('', validerTema);
     const melding = useFieldState('', validerMelding);
     const dialoger = useDialogContext();
-    const [NyDialogFormIsValid, setNyDialogFormValid] = useState();
+
+    function validerTema(tema: string): string | null {
+        if (tema.trim().length === 0) {
+            return 'Tema må ha innhold.';
+        } else {
+            return null;
+        }
+    }
+    function validerMelding(melding: string): string | null {
+        if (melding.trim().length === 0) {
+            return 'Melding må ha innhold.';
+        } else {
+            return null;
+        }
+    }
 
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
         tema.validate();
         melding.validate();
-        if (tema.input.feil === undefined && melding.input.feil === undefined) {
-            const body = JSON.stringify({
+        if (tema.input.feil !== undefined || melding.input.feil !== undefined) {
+            const nyDialogData: NyDialogMeldingData = {
+                dialogId: null,
                 overskrift: tema.input.value,
                 tekst: melding.input.value
-            });
+            };
+            const body = JSON.stringify(nyDialogData);
             fetchData<DialogData>('/veilarbdialog/api/dialog/ny', { method: 'post', body }).then(
                 function(response) {
                     console.log('Posted the new dialog!', response);
@@ -75,24 +52,62 @@ function DialogNew(props: Props) {
                 },
                 function(error) {
                     console.log('Failed posting the new dialog!', error);
-                    setNyDialogFormValid(['Failed establishing the new dialog!']);
                 }
             );
         }
     }
-
-    return (
-        <div className="dialog-new">
-            <form onSubmit={handleSubmit} noValidate>
-                <Innholdstittel className="dialog-new__tittel">Ny Dialog</Innholdstittel>
-                <Normaltekst className="dialog-new__infotekst">
-                    Her kan du skrive til din veileder om arbeid og oppfølging. Du vil få svar i løpet av noen dager.
-                </Normaltekst>
-                <Input className="dialog-new__temafelt" label={'Tema:'} placeholder="Skriv her" {...tema.input} />
-                <HenvendelseInput melding={melding} />
-            </form>
-        </div>
-    );
+    if (tema.input.feil !== undefined || melding.input.feil !== undefined) {
+        return (
+            <div className="dialog-new">
+                <form onSubmit={handleSubmit} noValidate>
+                    <Innholdstittel className="dialog-new__tittel">Ny Dialog</Innholdstittel>
+                    <Normaltekst className="dialog-new__infotekst">
+                        Her kan du skrive til din veileder om arbeid og oppfølging. Du vil få svar i løpet av noen
+                        dager.
+                    </Normaltekst>
+                    <div className="dialog-new__feedbacksummary">
+                        <Undertittel>Fyll ut obligatoriske felt</Undertittel>
+                        <ul>
+                            {tema.input.feil !== undefined ? (
+                                <li>
+                                    <Normaltekst className="dialog-new__errortekst">
+                                        {tema.input.feil.feilmelding}
+                                    </Normaltekst>
+                                </li>
+                            ) : (
+                                ''
+                            )}
+                            {melding.input.feil !== undefined ? (
+                                <li>
+                                    <Normaltekst className="dialog-new__errortekst">
+                                        {melding.input.feil.feilmelding}
+                                    </Normaltekst>
+                                </li>
+                            ) : (
+                                ''
+                            )}
+                        </ul>
+                    </div>
+                    <Input className="dialog-new__temafelt" label={'Tema:'} placeholder="Skriv her" {...tema.input} />
+                    <HenvendelseInput melding={melding} />
+                </form>
+            </div>
+        );
+    } else {
+        return (
+            <div className="dialog-new">
+                <form onSubmit={handleSubmit} noValidate>
+                    <Innholdstittel className="dialog-new__tittel">Ny Dialog</Innholdstittel>
+                    <Normaltekst className="dialog-new__infotekst">
+                        Her kan du skrive til din veileder om arbeid og oppfølging. Du vil få svar i løpet av noen
+                        dager.
+                    </Normaltekst>
+                    <Input className="dialog-new__temafelt" label={'Tema:'} placeholder="Skriv her" {...tema.input} />
+                    <HenvendelseInput melding={melding} />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default withRouter(DialogNew);
