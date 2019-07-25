e1c459a31dacd25d10f03fe2ec44fcd882d2c0cc
ingeborg.sollid <ingsollid@gmail.com>
2019-07-25 10:42:45 +0200

    FO-2412: Flytt datostempel og gitt tittel riktig fontweigth

--- src/view/DialogPreview.tsx
+++ src/view/DialogPreview.tsx
@@ -4,7 +4,7 @@ import { EtikettLiten, Normaltekst, Systemtittel } from 'nav-frontend-typografi'
 import { LenkepanelBase } from 'nav-frontend-lenkepanel';
 import { EtikettListe } from './EtikettListe';
 import './dialogpreview.less';
-import { formaterDateAndTime } from '../utils/date';
+import { formaterDate } from '../utils/date';
 import { ReactComponent as DialogIkon } from './Dialog_lest.svg';
 import { ReactComponent as AktivitetsIkon } from './aktivitet_lest.svg';
 import WrapInReactLink from '../component/hoc/wrapInReactLink';
@@ -23,17 +23,17 @@ function DialogPreviewIkon(props: Props) {
 }
 
 export function DialogPreview(props: Props) {
-    const datoString = !!props.dialog.sisteDato ? formaterDateAndTime(props.dialog.sisteDato) : '';
+    const datoString = !!props.dialog.sisteDato ? formaterDate(props.dialog.sisteDato) : '';
     const cls = classNames('dialog-preview', {
-        'dialog-preview--lest': !props.dialog.lest
+        'dialog-preview--lest': props.dialog.lest
     });
     return (
         <LenkepanelBase className={cls} href={`/${props.dialog.id}`} linkCreator={WrapInReactLink}>
             <DialogPreviewIkon dialog={props.dialog} />
             <div className="dialog-preview__internal-div">
-                <EtikettLiten>{datoString}</EtikettLiten>
                 <Systemtittel className="lenkepanel__heading"> {props.dialog.overskrift}</Systemtittel>
                 <Normaltekst className="dialog-preview__last-henvendelse">{props.dialog.sisteTekst}</Normaltekst>
+                <EtikettLiten>{datoString}</EtikettLiten>
                 <EtikettListe dialog={props.dialog} />
             </div>
         </LenkepanelBase>
