4a3b772e4cef2571606dc5606feeafb7369ef8f5
ingeborg.sollid <ingsollid@gmail.com>
2019-07-30 11:41:03 +0200

    FO-2422: Scroll til bunnen ved refresh og ny henvendelse sendes

--- src/view/HenvendelseList.tsx
+++ src/view/HenvendelseList.tsx
@@ -1,5 +1,5 @@
 import { HenvendelseData } from '../utils/typer';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Henvendelse } from './Henvendelse';
 
 import './henvendelseList.less';
@@ -9,19 +9,30 @@ interface Props {
 }
 
 export function HenvendelseList(props: Props) {
+    const endOfMessages = useRef<HTMLDivElement>(null);
+    const [isFirstRender, setIsFirstRender] = useState(true);
+
+    const scrolllToBottom = () => {
+        if (endOfMessages.current !== null) {
+            endOfMessages.current.scrollIntoView({ behavior: isFirstRender ? 'auto' : 'smooth' });
+            setIsFirstRender(false);
+        }
+    };
+    useEffect(scrolllToBottom, [props.henvendelseDataList.map(data => data.id)]);
+
     if (!props.henvendelseDataList) {
         return null;
     }
-
-    const henvendelser = props.henvendelseDataList.map(henvendelse => (
-        <div key={henvendelse.id} className="henvendelse-list__henvendelse">
-            <Henvendelse henvendelseData={henvendelse} />
-        </div>
-    ));
-
     return (
         <div className="henvendelse-list">
-            <div className="henvendelse-list__viewport">{henvendelser}</div>
+            <div className="henvendelse-list__viewport">
+                {props.henvendelseDataList.map(henvendelse => (
+                    <div key={henvendelse.id} className="henvendelse-list__henvendelse">
+                        <Henvendelse henvendelseData={henvendelse} />
+                    </div>
+                ))}
+                <div className="lastScroll" ref={endOfMessages} />
+            </div>
         </div>
     );
 }
