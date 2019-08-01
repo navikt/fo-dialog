6c0c78596528f64a35a7957c8c1830c771fb3d63
ingeborg.sollid <ingsollid@gmail.com>
2019-08-01 15:22:37 +0200

    FO-2422: Fjern unused variable

--- src/view/DialogOverviewHeader.tsx
+++ src/view/DialogOverviewHeader.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { visibleIfHoc } from '../component/hoc/visibleIfHoc';
-import { DialogData } from '../utils/typer';
 import { Link } from 'react-router-dom';
 import { ReactComponent as PlussIkon } from './ikon_pluss.svg';
 
-interface Props {
-    dialogData: DialogData[];
-}
-
-export function DialogOverviewHeader(props: Props) {
-    const brukerHarViktigeDialoger = !!props.dialogData.filter(dialog => dialog.egenskaper.length > 0).length;
-
+export function DialogOverviewHeader() {
     return (
         <div className="dialog-overview__header">
             <div className="header-panel">
