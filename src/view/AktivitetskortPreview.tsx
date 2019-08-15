cfa8d04e0836cda7aa3611576713d60a068024db
Yayla, Satilmis <satilmis.yayla@nav.no>
2019-08-15 15:26:43 +0200

    FO-2410: Mellomstor Aktivitetskort (OBS ved ca 550px bredde)

--- src/view/AktivitetskortPreview.tsx
+++ src/view/AktivitetskortPreview.tsx
@@ -5,27 +5,63 @@ import { Undertekst, Undertittel } from 'nav-frontend-typografi';
 import useFetch from '../utils/use-fetch';
 import { LenkepanelBase } from 'nav-frontend-lenkepanel';
 import { ReactComponent as AktivitetsIkon } from './aktivitet_lest.svg';
-import { formaterDate } from '../utils/date';
+import { formaterDate, konverterMinutterTilTimer } from '../utils/date';
+import Lenke from 'nav-frontend-lenker';
 
 interface Props {
     dialog: DialogData;
+}
+
+function mapAktivitetTypeToPreviw(aktivitet: Aktivitet): string {
+    switch (aktivitet.type) {
+        case 'STILLING':
+            return `Stilling | ${aktivitet.arbeidsgiver}`;
+        case 'IJOBB':
+            return `Jobb jeg har nå | ${aktivitet.arbeidsgiver}`;
+        case 'EGEN':
+            return `Jobbrettet egenaktivitet`;
+        case 'SOKEAVTALE':
+            return `${formaterDate(aktivitet.fraDato)} - ${formaterDate(aktivitet.tilDato)}`;
+        case 'BEHANDLING':
+            return aktivitet.behandlingType!;
+        case 'MOTE':
+            return `Møte med NAV | ${formaterDate(aktivitet.fraDato)} | ${konverterMinutterTilTimer(
+                aktivitet.klokkeslett!
+            )}`;
+        case 'SAMTALEREFERAT':
+            return `Samtalereferat | ${formaterDate(aktivitet.fraDato)}`;
+        case 'TILTAKSAKTIVITET':
+            return 'Tiltak gjennom NAV';
+        case 'GRUPPEAKTIVITET':
+            return 'Gruppeaktivitet';
+        case 'UTDANNING':
+            return 'Utdanning';
+    }
+
+    return '';
 }
 
 export function AktivitetskortPreview(props: Props) {
     const aktiviteter = useFetch<Aktivitet[]>('/veilarbaktivitet/api/aktivitet');
     if (aktiviteter.data !== null) {
         const aktivitet = aktiviteter.data.find(aktivitet => aktivitet.id === props.dialog.aktivitetId);
-        const datoString = !!props.dialog.sisteDato ? formaterDate(props.dialog.sisteDato) : '';
+        //const datoString = !!props.dialog.sisteDato ? formaterDate(props.dialog.sisteDato) : '';
 
         if (aktivitet) {
+            const info = mapAktivitetTypeToPreviw(aktivitet);
             return (
                 <LenkepanelBase href={'fisk'} className="aktivitetskortpreview">
                     <AktivitetsIkon className="aktivitetskortpreview__ikon" />
-                    <div>
-                        <Undertittel children={aktivitet.tittel} />
-                        <Undertekst>
-                            {datoString} | {aktivitet.arbeidsgiver}
-                        </Undertekst>
+                    <div className="aktivitetskortpreview__internal-div">
+                        <div className="textdiv">
+                            <Undertittel children={aktivitet.tittel} className="aktivitetskortpreview__info" />
+                            <Undertekst children={info} className="aktivitetskortpreview__info" />
+                        </div>
+                        <Lenke
+                            href={'aktivitetsplan'}
+                            className="aktivitetskortpreview__les-mer"
+                            children="Les mer i aktivitetsplanen"
+                        />
                     </div>
                 </LenkepanelBase>
             );
