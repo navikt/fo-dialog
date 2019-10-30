c490a398af0ec0af6efa790a2b9c4abc8faa8f28
Izis Frøyhov <izisfroyhov@gmail.com>
2019-10-30 10:11:09 +0100

    FO-2511: Splitte opp dialogkomponent

--- src/view/Routes.tsx
+++ src/view/Routes.tsx
@@ -2,12 +2,13 @@ import { Route, Switch } from 'react-router';
 import Dialog from './dialog/Dialog';
 import DialogNew from './dialog/NyDialog';
 import React from 'react';
+import DialogInfoMelding from './dialog/DialogInfoMelding';
 
 function Routes(props: { className: string }) {
     return (
         <div className={props.className}>
             <Switch>
-                <Route exact path="/" component={Dialog} />
+                <Route exact path="/" component={DialogInfoMelding} />
                 <Route path="/ny" component={DialogNew} />
                 <Route path="/:dialogId" component={Dialog} />
             </Switch>
