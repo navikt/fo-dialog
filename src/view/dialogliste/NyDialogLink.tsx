f85c7b50385e86ad720152bd6ec581e0725099fa
hakon planke holm <hakon.planke.holm@nav.no>
2020-01-07 10:52:51 +0100

    [FO-2653] ny dialoglink stylendring + nytt ikon

--- src/view/dialogliste/NyDialogLink.tsx
+++ src/view/dialogliste/NyDialogLink.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { visibleIfHoc } from '../../felleskomponenter/VisibleIfHoc';
 import { Link } from 'react-router-dom';
-import { ReactComponent as PlussIkon } from './pluss.svg';
+import { ReactComponent as PlussIkon } from './pluss_hvit.svg';
 import styles from './NyDialogLink.module.less';
 import classNames from 'classnames';
 
 export function NyDialogLink() {
-    const knappCls = classNames(styles.knapp, 'knapp--flat', 'knapp');
+    const knappCls = classNames(styles.knapp, 'knapp--hoved', 'knapp');
     return (
         <div className={styles.header}>
             <div className={styles.panel}>
                 <Link className={knappCls} to={'/ny'}>
-                    <PlussIkon />
+                    <PlussIkon className={styles.plusslogo} />
                     Ny dialog
                 </Link>
             </div>
