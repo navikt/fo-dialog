a4f942d17d88de1ba31446d0b157dfd28bdbd9f3
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-01-22 09:43:41 +0100

    FO-2627: Sett ReactModal.setAppElement på rett el

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from './view/App';
 import DemoBanner from './mock/demo/DemoBanner';
 import NAVSPA from '@navikt/navspa';
 import { erEksternBruker } from './mock/demo/sessionstorage';
-import ReactModal from 'react-modal';
 
 /*
 document.body.addEventListener('keydown', function(e: KeyboardEvent) {
@@ -17,9 +16,6 @@ document.body.addEventListener('keydown', function(e: KeyboardEvent) {
 });
 
 */
-
-const id = document.getElementById('pagewrapper') ? '#pagewrapper' : '#root';
-ReactModal.setAppElement(id);
 
 if (process.env.REACT_APP_MOCK === 'true') {
     require('./mock');
