4a8d257bfd785dccbeccdff830e9db4ce1408d47
hakon planke holm <hakon.planke.holm@nav.no>
2020-01-30 11:54:25 +0100

    [KAIZEN-0] baner ikon plassering ny dialog knapp for veileder stylfiks

--- src/view/dialogliste/NyDialogLink.tsx
+++ src/view/dialogliste/NyDialogLink.tsx
@@ -3,14 +3,12 @@ import { visibleIfHoc } from '../../felleskomponenter/VisibleIfHoc';
 import { Link } from 'react-router-dom';
 import { ReactComponent as PlussIkon } from './pluss.svg';
 import styles from './NyDialogLink.module.less';
-import classNames from 'classnames';
 
 export function NyDialogLink() {
-    const knappCls = classNames(styles.knapp, 'knapp--hoved', 'knapp');
     return (
         <div className={styles.header}>
             <div className={styles.panel}>
-                <Link className={knappCls} to={'/ny'}>
+                <Link className={styles.dialogKnapp} to={'/ny'}>
                     <PlussIkon className={styles.plusslogo} />
                     Ny dialog
                 </Link>
