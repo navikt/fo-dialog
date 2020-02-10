68dfa41d9950bd5a06c00df52da8ecd7c63de1c9
hakon planke holm <hakon.planke.holm@nav.no>
2020-02-10 14:38:57 +0100

    [KAIZEN-0] fiks tilbakemelding

--- src/utils/UseApiBasePath.ts
+++ src/utils/UseApiBasePath.ts
@@ -2,7 +2,7 @@ import { useFnrContext } from '../view/Provider';
 
 export default function useApiBasePath(): string {
     const fnr = useFnrContext();
-    return process.env.PUBLIC_URL && !fnr ? process.env.PUBLIC_URL : '';
+    return baseApiPath(fnr);
 }
 
 export function baseApiPath(fnr?: string) {
