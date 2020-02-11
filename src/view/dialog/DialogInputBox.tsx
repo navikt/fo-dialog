9a3f16e87ae544c563d8eafcace36a9443f5eb00
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-02-11 14:24:35 +0100

    KAIZEN-0: Fjern autofocus
    Ser rart ut på mobil

--- src/view/dialog/DialogInputBox.tsx
+++ src/view/dialog/DialogInputBox.tsx
@@ -90,7 +90,6 @@ export function DialogInputBox(props: Props) {
             <form onSubmit={state.onSubmit(onSubmit)} noValidate autoComplete="off">
                 <div className="skriv-melding label-sr-only">
                     <Textarea
-                        autoFocus
                         label="Skriv en melding om arbeid og oppfølging"
                         placeholder="Skriv en melding om arbeid og oppfølging"
                         textareaClass="autosizing-textarea"
