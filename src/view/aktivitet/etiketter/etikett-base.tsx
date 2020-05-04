02d2fff723660d73dd0b8e807417e48f0611b512
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-05-04 17:37:37 +0200

    FO-2879: Fjern ubrukt import

--- src/view/aktivitet/etiketter/etikett-base.tsx
+++ src/view/aktivitet/etiketter/etikett-base.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Normaltekst } from 'nav-frontend-typografi';
 import styles from './etikett.module.less';
 
 interface Props {
