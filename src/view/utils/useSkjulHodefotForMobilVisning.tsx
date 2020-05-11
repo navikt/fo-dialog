30a1b177fed0a4de13423888c908a9f1320dfcfa
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-05-11 13:49:17 +0200

    FO-2893: Fix hodefot select for ny decoratør

--- src/view/utils/useSkjulHodefotForMobilVisning.tsx
+++ src/view/utils/useSkjulHodefotForMobilVisning.tsx
@@ -1,15 +1,15 @@
 import { useLayoutEffect } from 'react';
 
-const selectHodefot = () => Array.from(document.querySelectorAll('.hodefot'));
+const selectHodefot = () => Array.from(document.querySelectorAll('.hodefot, #decorator-header, #decorator-footer'));
 
 function skjulElementerForMobil() {
-    selectHodefot().forEach(el => {
+    selectHodefot().forEach((el) => {
         el.classList.add('hide-on-smaller-then-md');
     });
 }
 
 function visElementer() {
-    selectHodefot().forEach(el => {
+    selectHodefot().forEach((el) => {
         el.classList.remove('hide-on-smaller-then-md');
     });
 }
