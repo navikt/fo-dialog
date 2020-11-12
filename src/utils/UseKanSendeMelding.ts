02607b90b42aca02bb1f13ef5da7fbd57f492e79
hakon planke holm <hakon.planke.holm@nav.no>
2020-11-12 14:16:33 +0100

    nivaa 4 hack

--- src/utils/UseKanSendeMelding.ts
+++ src/utils/UseKanSendeMelding.ts
@@ -1,14 +1,17 @@
-import { dataOrUndefined, useHarNivaa4Context, useOppfolgingContext } from '../view/Provider';
+import { dataOrUndefined, useOppfolgingContext } from '../view/Provider';
 import { erProd } from './FellesFunksjoner';
 
 export default function useKansendeMelding(): boolean {
     const oppfolgingContext = useOppfolgingContext();
-    const harNivaa4 = useHarNivaa4Context();
+    //TODO min id pasport
+    //const harNivaa4 = useHarNivaa4Context();
     const oppfolgingData = dataOrUndefined(oppfolgingContext);
 
     if (!oppfolgingData) {
         return false;
     }
+
+    //TODO sjekk denne mot doly brukere
 
     // erProd trengs da ingen av brukerne er registrert i krr i testmiljø
     const kanVarsles = oppfolgingData.kanVarsles || !erProd();
@@ -18,7 +21,9 @@ export default function useKansendeMelding(): boolean {
         oppfolgingData.underOppfolging &&
         !oppfolgingData.reservasjonKRR &&
         kanVarsles &&
-        !oppfolgingData.manuell &&
-        harNivaa4.harNivaa4
+        !oppfolgingData.manuell
+        //TODO min id pasport  er ferdig
+        //&&
+        //harNivaa4.harNivaa4
     );
 }
