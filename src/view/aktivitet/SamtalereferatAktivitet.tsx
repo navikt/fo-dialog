40a35445ae77f40475765fdba33dfed04b4d770c
Cato Olsen <cato.olsen@nav.no>
2021-06-24 15:43:48 +0200

    Bruk useUserInfoContext() for å avgjøre om bruker er veileder eller ekstern.

--- src/view/aktivitet/SamtalereferatAktivitet.tsx
+++ src/view/aktivitet/SamtalereferatAktivitet.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { SamtalereferatIkkeDelt } from '../../felleskomponenter/etiketer/Etikett';
-import { erEksternBruker } from '../../mock/demo/sessionstorage';
 import { Aktivitet } from '../../utils/aktivitetTypes';
 import { formaterDate } from '../../utils/Date';
+import { useUserInfoContext } from '../Provider';
 import InformasjonElement, { InformasjonElementRaw } from './InformasjonElement';
 import { getKanalTekst } from './MoteAktivitet';
 
@@ -14,12 +14,14 @@ interface PropTypes {
 export default function SamtalereferatAktivitet(props: PropTypes) {
     const { fraDato, kanal, referat, erReferatPublisert } = props.aktivitet;
 
+    const bruker = useUserInfoContext();
+    const erVeileder = !!bruker?.erVeileder;
     const referatPublisertElement = <InformasjonElement merkelapptekst="Referat" verdi={referat} />;
-    const referatIkkePublisertElement = erEksternBruker() ? null : (
+    const referatIkkePublisertElement = erVeileder ? (
         <InformasjonElementRaw merkelapptekst="Referat">
-            {SamtalereferatIkkeDelt({ visible: !erEksternBruker() })}
+            {SamtalereferatIkkeDelt({ visible: true })}
         </InformasjonElementRaw>
-    );
+    ) : null;
     const referatElement = erReferatPublisert ? referatPublisertElement : referatIkkePublisertElement;
 
     return (
