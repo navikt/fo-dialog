90458e9a51edee51b672963b2317698ff7e82df6
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-07-08 09:45:52 +0200

    Oppdatere til nye farger i designsystemet (#343)
    * Oppdatere til nye farger i designsystemet
    
    * Apply formatting changes
    
    * Oppdatere readme
    
    Co-authored-by: Prettier bot <dab@nav.no>

--- src/mock/Dialog.ts
+++ src/mock/Dialog.ts
@@ -133,8 +133,7 @@ const dialoger: DialogData[] & JSONArray = [
                 sendt: '2018-02-01T11:52:20.615+01:00',
                 lest: true,
                 viktig: true,
-                tekst:
-                    'Det er viktig at du gjennomfører denne aktiviteten med NAV. Gjør du ikke det, kan det medføre at stønaden du mottar fra NAV bortfaller for en periode eller stanses. Hvis du ikke kan gjennomføre aktiviteten, ber vi deg ta kontakt med veilederen din så snart som mulig.'
+                tekst: 'Det er viktig at du gjennomfører denne aktiviteten med NAV. Gjør du ikke det, kan det medføre at stønaden du mottar fra NAV bortfaller for en periode eller stanses. Hvis du ikke kan gjennomføre aktiviteten, ber vi deg ta kontakt med veilederen din så snart som mulig.'
             }
         ],
         egenskaper: ['ESKALERINGSVARSEL']
@@ -395,8 +394,7 @@ const dialoger: DialogData[] & JSONArray = [
                 sendt: '2018-02-01T11:52:20.615+01:00',
                 lest: false,
                 viktig: false,
-                tekst:
-                    'Hei. Jeg vil at du skal prøve å søke minst 5 stillinger i uken. Vi møtes igjen om en uke for å prate om hvordan det har gått. '
+                tekst: 'Hei. Jeg vil at du skal prøve å søke minst 5 stillinger i uken. Vi møtes igjen om en uke for å prate om hvordan det har gått. '
             }
         ],
         egenskaper: []
@@ -424,8 +422,7 @@ const dialoger: DialogData[] & JSONArray = [
                 sendt: '2018-02-01T11:52:20.615+01:00',
                 lest: false,
                 viktig: false,
-                tekst:
-                    'Hei. Jeg vil at du skal prøve å søke minst 5 stillinger i uken. Vi møtes igjen om en uke for å prate om hvordan det har gått. '
+                tekst: 'Hei. Jeg vil at du skal prøve å søke minst 5 stillinger i uken. Vi møtes igjen om en uke for å prate om hvordan det har gått. '
             }
         ],
         egenskaper: []
@@ -572,7 +569,9 @@ export function oppdaterKladd(data: KladdData) {
     return null;
 }
 
-export default () => {
+const dialog = () => {
     opprettDialogEtterRender();
     return harIngenDialoger() ? [] : [...dialoger];
 };
+
+export default dialog;
