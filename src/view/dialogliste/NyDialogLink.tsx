bdea2eb7dbac5fe9cff9c3a09460f684a06450a9
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-07-09 14:26:08 +0200

    Ta tilbake gammel knapp

--- src/view/dialogliste/NyDialogLink.tsx
+++ src/view/dialogliste/NyDialogLink.tsx
@@ -1,20 +1,17 @@
-import { Hovedknapp } from 'nav-frontend-knapper';
 import React from 'react';
-import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import { visibleIfHoc } from '../../felleskomponenter/VisibleIfHoc';
 import styles from './NyDialogLink.module.less';
 import { ReactComponent as PlussIkon } from './pluss.svg';
 
 const NyDialogLink = () => {
-    const history = useHistory();
-
     return (
         <div className={styles.header}>
-            <Hovedknapp kompakt onClick={() => history.push('/ny')}>
+            <Link className={styles.dialogKnapp} to={'/ny'}>
                 <PlussIkon className={styles.plusslogo} />
-                <span>Ny dialog</span>
-            </Hovedknapp>
+                Ny dialog
+            </Link>
         </div>
     );
 };
