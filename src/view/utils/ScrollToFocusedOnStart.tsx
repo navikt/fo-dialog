d6fb6af49c79e2606f90e6862f58ebe1c05e8414
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-10-13 12:08:23 +0200

    logging props when ScrollToFocusedOnStart renders

--- src/view/utils/ScrollToFocusedOnStart.tsx
+++ src/view/utils/ScrollToFocusedOnStart.tsx
@@ -2,7 +2,13 @@ import React, { ReactElement, useEffect } from 'react';
 import { RouteComponentProps, RouteProps, withRouter } from 'react-router-dom';
 
 const ScrollToFocusedOnStart = (props: RouteProps): ReactElement<RouteComponentProps<any>> => {
-    useEffect(() => document.activeElement?.scrollIntoView(false), []);
+    useEffect(() => {
+        console.log({ props });
+    }, [props]);
+
+    useEffect(() => {
+        document.activeElement?.scrollIntoView(false);
+    }, []);
     return <>{props.children}</>;
 };
 
