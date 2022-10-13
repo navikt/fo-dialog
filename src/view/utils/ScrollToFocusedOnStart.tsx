805367c52359fedeb0759967f8052e4522408079
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-10-13 12:40:46 +0200

    logging

--- src/view/utils/ScrollToFocusedOnStart.tsx
+++ src/view/utils/ScrollToFocusedOnStart.tsx
@@ -3,10 +3,12 @@ import { RouteComponentProps, RouteProps, withRouter } from 'react-router-dom';
 
 const ScrollToFocusedOnStart = (props: RouteProps): ReactElement<RouteComponentProps<any>> => {
     useEffect(() => {
+        console.log('ScrollToFocusedOnStart props changed');
         console.log({ props });
     }, [props]);
 
     useEffect(() => {
+        console.log('ScrollToFocusedOnStart componentDidMount(?)');
         document.activeElement?.scrollIntoView(false);
     }, []);
     return <>{props.children}</>;
