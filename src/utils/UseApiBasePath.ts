9da17876ea6e7dc8a352d617923946530c2313ed
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-12-05 11:01:25 +0100

    pathnameprefix er alltid public_url

--- src/utils/UseApiBasePath.ts
+++ src/utils/UseApiBasePath.ts
@@ -1,11 +1,3 @@
-import { runningOnGithubPages } from '../view/utils/utils';
-
-export function getPathnamePrefix(erVeileder: boolean) {
+export function getPathnamePrefix(_erVeileder: boolean) {
     return process.env.PUBLIC_URL;
-    // if (runningOnGithubPages) return process.env.PUBLIC_URL; // Only used in testing purposes
-    // if (erVeileder) {
-    //     return '';
-    // } else {
-    //     return process.env.PUBLIC_URL || '';
-    // }
 }
