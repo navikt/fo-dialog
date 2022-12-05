b4b73c0ce47895dc19698c47f3d417a34a52acfa
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-12-05 15:18:07 +0100

    fjerner console.log og setter systemvariabel for prod

--- src/utils/UseApiBasePath.ts
+++ src/utils/UseApiBasePath.ts
@@ -1,4 +1,9 @@
 export const pathnamePrefix = process.env.PUBLIC_URL;
+
+// Vi trenger å vite om applikasjonen skal kjøres uten /arbeid/dialog
+// Vi antar at alle api-kall skl prefikses med public_url
+// Dette er løst ved å sette REACT_APP_BASEAPIURL som overstyrer PUBLIC_URL
+// OBS: public_url kan komme fra "homepage" i package.json
 export const baseApiUrl = process.env.REACT_APP_BASEAPIURL || pathnamePrefix;
 export function getPathnamePrefix() {
     return baseApiUrl === '/' ? '' : baseApiUrl;
