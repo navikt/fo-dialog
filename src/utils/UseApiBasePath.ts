7142e56419ced8c7b5262137bb5a4ea052ef5b42
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-12-05 14:57:02 +0100

    setter ikke inn dobbel slash

--- src/utils/UseApiBasePath.ts
+++ src/utils/UseApiBasePath.ts
@@ -1,5 +1,5 @@
 export const pathnamePrefix = process.env.PUBLIC_URL;
 export const baseApiUrl = process.env.REACT_APP_BASEAPIURL || pathnamePrefix;
 export function getPathnamePrefix() {
-    return baseApiUrl;
+    return baseApiUrl === '/' ? '' : baseApiUrl;
 }
