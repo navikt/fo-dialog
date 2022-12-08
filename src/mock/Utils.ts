1dc076a624708013fa4dedd816fba036eb6b0864
sigurdgroneng <sigurd.groneng@nav.no>
2022-12-08 14:20:54 +0100

    Fix redirecting in mock-app

--- src/mock/Utils.ts
+++ src/mock/Utils.ts
@@ -9,9 +9,26 @@ export function rndId() {
     return `${Math.floor(array[0] % 100_000_000)}`;
 }
 
+const isNineDigits = (val: string) => /^\d{9}$/.test(val);
+export const toggleFnrInUrl = (hashPart: string, fnr?: string) => {
+    const parts = hashPart.split('/').slice(1);
+    if (fnr && parts[0] === fnr) {
+        return '#/' + parts.join('/');
+    } else if (fnr && parts[0] !== fnr) {
+        return '#/' + [fnr, ...parts].join('/');
+    } else if (!fnr && parts.length === 0) {
+        return '#/';
+    } else if (!fnr && isNineDigits(parts[0].toString())) {
+        return '#/' + parts.slice(1).join('/');
+    } else {
+        return '#/';
+    }
+};
+
 export const gotoStartTestPage = (fnr?: string) => {
     if (runningOnGithubPages) {
-        window.history.replaceState({}, '', settSammenmedSlasher(pathnamePrefix, '#', fnr));
+        const hashPartOfUrl = toggleFnrInUrl(window.location.hash, fnr);
+        window.history.replaceState({}, '', settSammenmedSlasher(pathnamePrefix, '#', hashPartOfUrl));
     } else {
         window.history.replaceState({}, '', settSammenmedSlasher(pathnamePrefix, fnr));
     }
