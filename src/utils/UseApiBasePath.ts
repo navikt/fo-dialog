c9d7bd26a93525ebf9dbe9babfdd0f138fb4e62e
sigurdgroneng <sigurd.groneng@nav.no>
2022-12-08 10:27:56 +0100

    Pulic url kan ikke være bare slash

--- src/utils/UseApiBasePath.ts
+++ src/utils/UseApiBasePath.ts
@@ -1,7 +1,10 @@
-export const pathnamePrefix = process.env.PUBLIC_URL;
+const tomStrengHvisBaseSlash = (value: string) => {
+    return value === '/' ? '' : value;
+};
+export const pathnamePrefix = tomStrengHvisBaseSlash(process.env.PUBLIC_URL);
 // Api url skal være PUBLIC_URL eksternt, tom strent internt
 const getApiBasePath = () => {
     const unprocessedPath = process.env.REACT_APP_BASEAPIURL || pathnamePrefix;
-    return unprocessedPath === '/' ? '' : unprocessedPath;
+    return tomStrengHvisBaseSlash(unprocessedPath);
 };
 export const apiBasePath = getApiBasePath();
