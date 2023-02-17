7a3a8c466da802e965bb225bb4650fafe6a08ab0
sigurdgroneng <sigurd.groneng@nav.no>
2023-02-17 12:43:40 +0100

    Update snapshot tests

--- src/view/aktivitet/AktivitetskortLenke.tsx
+++ src/view/aktivitet/AktivitetskortLenke.tsx
@@ -3,12 +3,15 @@ import { Element } from 'nav-frontend-typografi';
 import React, { MouseEvent } from 'react';
 
 import { aktivtetsplanUrl } from '../../metrics/constants';
-import { apiBasePath } from '../../utils/UseApiBasePath';
 import { useFnrContext } from '../Provider';
 import { getContextPath } from '../utils/utils';
 import styles from './Aktivitetskort.module.less';
 
-export const aktivitetLenke = (aktivitetId: string) => `${aktivtetsplanUrl}/aktivitet/vis/${aktivitetId}`;
+export const aktivitetLenke = (aktivitetId: string) => {
+    console.log(process.env);
+    console.log({ aktivtetsplanUrl, lol: process.env.REACT_APP_AKTIVITETSPLAN_URL });
+    return `${aktivtetsplanUrl}/aktivitet/vis/${aktivitetId}`;
+};
 
 export const visAktivitetsplan = (aktivitetID: string, fnrContext?: string) => (event: MouseEvent) => {
     if (!fnrContext) {
