0d424dbc679b74941c7fa0afd1a714d515451f1d
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-04-28 16:27:51 +0200

    remove trailing slash from baseurl

--- src/utils/UseApiBasePath.ts
+++ src/utils/UseApiBasePath.ts
@@ -1,15 +1,15 @@
 import { USE_HASH_ROUTER } from '../constants';
 
-const tomStrengHvisBaseSlash = (value: string) => {
-    return value === '/' ? '' : value;
+const stripTrailingSlash = (baseUrl: string) => {
+    return baseUrl.endsWith('/') ? baseUrl.substring(0, baseUrl.length - 1) : baseUrl;
 };
-export const pathnamePrefix = tomStrengHvisBaseSlash(import.meta.env.BASE_URL);
+
+export const pathnamePrefix = stripTrailingSlash(import.meta.env.BASE_URL);
 
 const getApiBasePath = () => {
     if (USE_HASH_ROUTER) {
         return '';
-    } // ghpages
-    const unprocessedPath = import.meta.env.BASE_URL || pathnamePrefix;
-    return tomStrengHvisBaseSlash(unprocessedPath);
+    }
+    return pathnamePrefix;
 };
 export const apiBasePath = getApiBasePath();
