7973f9b6497f50d9d37a5036edbb0078752cddf7
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-05-04 10:17:14 +0200

    refac

--- src/index.tsx
+++ src/index.tsx
@@ -3,37 +3,29 @@ import './polyfill';
 import { Modal } from '@navikt/ds-react';
 import NAVSPA from '@navikt/navspa';
 // This is to size the window correctly
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 import { USE_MOCK } from './constants';
-// import { initAmplitude } from './metrics/amplitude-utils';
+import { initAmplitude } from './metrics/amplitude-utils';
 import DemoBanner from './mock/demo/DemoBanner';
 import { erEksternBruker } from './mock/demo/localstorage';
 import { gotoStartTestPage } from './mock/Utils';
 
-const modalAlly = document.getElementById('modal-a11y-wrapper');
-const root = document.getElementById('root');
-
-// TODO: Only use this in dev?
-// initAmplitude();
-
-Modal.setAppElement(modalAlly ? document.getElementById('modal-a11y-wrapper') : root);
-
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener(
-    'resize',
-    throttle(() => {
-        // We execute the same script as before
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    }, 100)
-);
+// let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+//
+// // We listen to the resize event
+// window.addEventListener(
+//     'resize',
+//     throttle(() => {
+//         // We execute the same script as before
+//         document.documentElement.style.setProperty('--vh', `${vh}px`);
+//     }, 100)
+// );
 
 const AppWebComponent = ({ fnr }: { fnr: string }) => {
     return React.createElement('dab-dialog', {
@@ -52,15 +44,16 @@ const exportToNavSpa = () => {
 
 const renderAsRootApp = (fnr?: string) => {
     const rootElement = document.getElementById('root');
+    Modal.setAppElement(rootElement);
     ReactDOM.render(<App fnr={fnr} key={'1'} />, rootElement as HTMLElement);
 };
 
 const renderApp = (fnr?: string) => {
-    // if (['dev-intern', 'prod-intern'].includes(import.meta.env.MODE)) {
-    exportToNavSpa();
-    // } else {
-    //     renderAsRootApp(fnr);
-    // }
+    if (['dev-intern', 'prod-intern'].includes(import.meta.env.MODE)) {
+        exportToNavSpa();
+    } else {
+        renderAsRootApp(fnr);
+    }
 };
 
 if (USE_MOCK) {
@@ -76,5 +69,6 @@ if (USE_MOCK) {
             renderApp(fnr);
         });
 } else {
+    initAmplitude();
     renderApp();
 }
