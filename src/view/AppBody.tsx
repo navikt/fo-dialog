857233061aeae44bd628e4253d77cebaf1a52f82
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-05-11 15:53:46 +0200

    fix flex-grow growing outside parent height

--- src/view/AppBody.tsx
+++ src/view/AppBody.tsx
@@ -59,7 +59,7 @@ const AppBody = () => {
             <DialogOversikt />
             <div className="h-full flex flex-col flex-1">
                 <DialogHeader />
-                <div className="flex flex-1">
+                <div className="flex flex-1 min-h-0">
                     <Outlet />
                 </div>
             </div>
