4196c14f80f6ccc7d69e16807334e630313c55e3
Marc <rhyor420@gmail.com>
2023-05-16 13:59:34 +0200

    remove polyfill, delete commented code (#454)

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,12 +1,2 @@
 import 'core-js/stable';
 import 'whatwg-fetch';
-
-// @ts-ignore
-// import smoothscroll from 'smoothscroll-polyfill';
-
-// @ts-ignore
-// if (!window._babelPolyfill) {
-//     require('babel-polyfill');
-// }
-
-// smoothscroll.polyfill();
