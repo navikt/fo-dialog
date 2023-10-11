6b97816bbeb085133c9bb54115511ea272751878
johannetronstad <johanne.tronstad@nav.no>
2023-10-11 13:41:40 +0200

    fix

--- src/view/aktivitet/Aktivitetskort.tsx
+++ src/view/aktivitet/Aktivitetskort.tsx
@@ -16,6 +16,8 @@ export function Aktivitetskort() {
     const compactMode = useCompactMode();
     const visAktivitet = useVisAktivitet();
     const harDialog = useSelectedDialog();
+    console.log('har dialog: ', harDialog);
+    console.log('aktivitet: ', aktivitet);
 
     if (!aktivitet) {
         if (compactMode) {
@@ -30,7 +32,7 @@ export function Aktivitetskort() {
     return (
         <div
             className={classNames(' w-full border-l border-border-divider lg:max-w-[320px] xl:max-w-screen-w-1/3', {
-                'lg:flex hidden': !harDialog && !visAktivitet,
+                'lg:flex hidden': !harDialog && aktivitet,
                 hidden: !visAktivitet,
                 ' lg:flex hidden': visAktivitet,
                 '2xl:flex 2xl:max-w-screen-w-1/4': compactMode,
