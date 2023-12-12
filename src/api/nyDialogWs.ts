4813c32ce228190b67435ef0a8101be121123e5a
sigurdgroneng <sigurd.groneng@nav.no>
2023-12-12 12:34:12 +0100

    Fix code smells

--- src/api/nyDialogWs.ts
+++ src/api/nyDialogWs.ts
@@ -1,5 +1,3 @@
-import { fetchData } from '../utils/Fetch';
-
 const PLEASE_URL = 'please.ekstern.dev.nav.no';
 const ticketUrl = `/please/ws-auth-ticket`;
 const socketUrl = `ws://${PLEASE_URL}/ws`;
