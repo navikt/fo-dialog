1a5490cb39b781a17443e102972ef01328596b1a
sigurdgroneng <sigurd.groneng@nav.no>
2024-09-03 08:38:00 +0200

    Bruk sentry

--- src/rootWrapper.tsx
+++ src/rootWrapper.tsx
@@ -5,8 +5,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import { createBrowserRouter } from 'react-router-dom';
-import './faro';
-// import './sentry2';
+// import './faro';
+import './sentry2';
 // import { sentryCreateBrowserRouter } from './sentry2';
 
 export const renderAsReactRoot = (fnr?: string) => {
--- src/sentry2.tsx
+++ src/sentry2.tsx
@@ -60,10 +60,8 @@ Sentry.init({
     environment: getEnv(),
     enabled: getEnv() !== Env.Local,
     ignoreErrors: [
-        /^Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.$/,
-        /^Uventet feil fra dekoratøren: NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node. \[object Object]$/,
-        /^Uventet feil fra dekoratøren: NotFoundError: The object can not be found here. \[object Object]$/,
-        /^The object can not be found here.$/
+        /Ampltidude/
+        // /^Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.$/,
     ],
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
