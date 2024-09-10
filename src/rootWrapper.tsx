d773e655694d52abb3de70656ad247b274ed7b06
sigurdgroneng <sigurd.groneng@nav.no>
2024-09-10 11:55:46 +0200

    Actually use sentry router wrapper

--- src/rootWrapper.tsx
+++ src/rootWrapper.tsx
@@ -4,12 +4,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import { createBrowserRouter } from 'react-router-dom';
-// import './faro';
 import './sentry2';
-// import { sentryCreateBrowserRouter } from './sentry2';
+import { sentryCreateBrowserRouter } from './sentry2';
 
 export const renderAsReactRoot = () => {
     const rootElement = document.getElementById('root');
-    createRoot(rootElement!).render(<App createRouter={createBrowserRouter} />);
+    createRoot(rootElement!).render(<App createRouter={sentryCreateBrowserRouter} />);
 };
