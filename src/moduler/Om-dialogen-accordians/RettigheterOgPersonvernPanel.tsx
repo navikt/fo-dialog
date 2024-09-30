bbb380321a0d24b1649df27e90408b3e6a6a9c49
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-09-30 13:22:52 +0200

    Gjort om til accordian på Om dialogen og lagt til innsynsrett info/under 18

---
+++ src/moduler/Om-dialogen-accordians/RettigheterOgPersonvernPanel.tsx
@@ -0,0 +1,57 @@
+import { Accordion, BodyShort, Heading, Link } from '@navikt/ds-react';
+import React, { useState } from 'react';
+
+export function RettigheterPanel() {
+    const [open, setOpen] = useState(false);
+
+    function handleClick() {
+        setOpen(!open);
+    }
+
+    return (
+        <Accordion.Item>
+            <Accordion.Header onClick={handleClick}>
+                <Heading level="2" size="small">
+                    Rettigheter og personvern
+                </Heading>
+            </Accordion.Header>
+            <Accordion.Content>
+                <BodyShort>
+                    Informasjon du gir i meldinger til veilederen
+                    din brukes til å vurdere behovet ditt for hjelp fra NAV.
+                </BodyShort>
+                <Heading level="3" size="xsmall" className="pt-4">
+                    Manuell oppfølging
+                </Heading>
+                <BodyShort className="pb-4">
+                    NAV henter informasjon om deg fra Folkeregisteret og
+                    sjekker mot Kontakt- og reservasjonsregisteret.
+                </BodyShort>
+                <BodyShort>
+                    Hvis du ikke ønsker å bruke den digitale dialogen,
+                    så kan du reservere deg mot digital kommunikasjon med det offentlige hos Norge.
+                    no Hvis du reserverer deg mot digital kommunikasjon, vil NAV følge deg opp manuelt.
+                </BodyShort>
+
+                <Heading level="3" size="xsmall" className="pt-4">
+                    Deling og lagring
+                </Heading>
+                <BodyShort className="pb-4">
+                    Opplysningene i dialogen og aktivitetsplanen blir ikke delt med andre offentlige etater,
+                    med mindre de har rett til å hente slike opplysninger.
+                </BodyShort>
+                <BodyShort className="pb-4">
+                    Opplysningene i dialogen og aktivitetsplanen blir lagret og oppbevart etter arkivloven.
+                    Meldinger i dialogen kan ikke slettes når de først er opprettet.
+                </BodyShort>
+                <BodyShort>
+                    Les mer om{' '}
+                    <Link href="https://www.nav.no/no/NAV+og+samfunn/Om+NAV/personvern-i-arbeids-og-velferdsetaten">
+                        hvordan NAV behandler personopplysninger
+                    </Link>
+                    .
+                </BodyShort>
+            </Accordion.Content>
+        </Accordion.Item>
+    );
+}
