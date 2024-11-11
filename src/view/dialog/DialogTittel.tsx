5d3df40aa11d2cf903d372ba797ac39b38ae0263
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-11-11 08:31:33 +0100

    testing for focus mellom apper

--- src/view/dialog/DialogTittel.tsx
+++ src/view/dialog/DialogTittel.tsx
@@ -1,7 +1,6 @@
 import { Heading } from '@navikt/ds-react';
 import React, { FunctionComponent, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-
 interface Props {
     tittel: string | null | undefined;
     ariaLabel?: string;
@@ -12,12 +11,12 @@ export const DialogTittel: FunctionComponent<Props> = ({ tittel, ariaLabel }) =>
     const { state } = useLocation();
 
     useEffect(() => {
-        if (headerRef.current && state && state.fokuspaHedear) {
+        if (headerRef.current && state && state.fokuspaHedear || headerRef.current && tittel) {
             headerRef.current.focus();
             const newState = { ...state, fokuspaHedear: false };
             window.history.replaceState(newState, document.title, window.location.href);
         }
-    }, [state]);
+    }, [state || tittel]);
 
     if (!tittel) {
         return null;
