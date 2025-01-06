cde765e8c57152bf15ef23ae945d108586219a4c
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-06 13:43:16 +0100

    lagt til riktig tekst om bruker er reservert krr

--- src/view/statusAdvarsel/ReservertKrr.tsx
+++ src/view/statusAdvarsel/ReservertKrr.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@navikt/ds-react';
+import { Link, Heading } from '@navikt/ds-react';
 import React from 'react';
 
 import StatusAdvarselWrapper, { KanIkkeKontakteElektroniskVeileder } from './StatusAdvarselWrapper';
@@ -14,8 +14,11 @@ export default function ReservertKrr(props: Props) {
 function BrukerKrr() {
     return (
         <StatusAdvarselWrapper>
-            For å ta i bruk den digitale dialogen med din veileder, må du fjerne reservasjonen din mot digital
-            kommunikasjon.
+            <Heading spacing size="small" level="3">
+            Du har reservert deg mot digital kommunikasjon
+            </Heading>
+            Du kan ikke sende meldinger i den digitale dialogen fordi
+            du har reservert deg mot digital kommunikasjon i kontakt og reservasjonsregisteret (KRR).
             <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
                 Gå til Norge.no for å fjerne reservasjonen
             </Link>
