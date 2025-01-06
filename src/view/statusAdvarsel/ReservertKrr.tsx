5b3478ab9c65abb7af130618130199ba6b483bbd
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-06 15:07:00 +0100

    riktig melding om bruker ikke er i krr

--- src/view/statusAdvarsel/ReservertKrr.tsx
+++ src/view/statusAdvarsel/ReservertKrr.tsx
@@ -2,6 +2,7 @@ import { Link, Heading } from '@navikt/ds-react';
 import React from 'react';
 
 import StatusAdvarselWrapper, { KanIkkeKontakteElektroniskVeileder } from './StatusAdvarselWrapper';
+import { erKRRBruker } from '../../mock/demo/localstorage';
 
 interface Props {
     erVeileder: boolean;
@@ -12,16 +13,32 @@ export default function ReservertKrr(props: Props) {
 }
 
 function BrukerKrr() {
-    return (
-        <StatusAdvarselWrapper>
-            <Heading spacing size="small" level="3">
-            Du har reservert deg mot digital kommunikasjon
-            </Heading>
-            Du kan ikke sende meldinger i den digitale dialogen fordi
-            du har reservert deg mot digital kommunikasjon i kontakt og reservasjonsregisteret (KRR).
-            <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
-                Gå til Norge.no for å fjerne reservasjonen
-            </Link>
-        </StatusAdvarselWrapper>
-    );
+   if (!erKRRBruker()) {
+       return (
+           <StatusAdvarselWrapper>
+               <Heading spacing size="small" level="3">
+                   Vi har ikke din kontaktinformasjon
+               </Heading>
+               Du kan ikke sende meldinger i dialogen fordi du ikke har registrert
+               e-post eller telefonnummeret ditt i kontakt og reservasjonsregisteret (KRR).
+               <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
+                   Gå til norge.no for å registrere.
+               </Link>
+           </StatusAdvarselWrapper>
+       )
+   }
+else {
+       return (
+           <StatusAdvarselWrapper>
+               <Heading spacing size="small" level="3">
+                   Du har reservert deg mot digital kommunikasjon
+               </Heading>
+               Du kan ikke sende meldinger i den digitale dialogen fordi
+               du har reservert deg mot digital kommunikasjon i kontakt og reservasjonsregisteret (KRR).
+               <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
+                   Gå til Norge.no for å fjerne reservasjonen
+               </Link>
+           </StatusAdvarselWrapper>
+       );
+   }
 }
