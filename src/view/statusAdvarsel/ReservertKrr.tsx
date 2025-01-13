faec9936b81f1fcfa35809201d6a2bca60dd0801
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-13 11:42:00 +0100

    reservert krr

--- src/view/statusAdvarsel/ReservertKrr.tsx
+++ src/view/statusAdvarsel/ReservertKrr.tsx
@@ -2,7 +2,6 @@ import { Link, Heading } from '@navikt/ds-react';
 import React from 'react';
 
 import StatusAdvarselWrapper, { KanIkkeKontakteElektroniskVeileder } from './StatusAdvarselWrapper';
-import { erKRRBruker } from '../../mock/demo/localstorage';
 
 interface Props {
     erVeileder: boolean;
@@ -13,7 +12,6 @@ export default function ReservertKrr(props: Props) {
 }
 
 function BrukerKrr() {
-   if (!erKRRBruker()) {
        return (
            <StatusAdvarselWrapper>
                <Heading spacing size="small" level="3">
@@ -27,18 +25,3 @@ function BrukerKrr() {
            </StatusAdvarselWrapper>
        )
    }
-else {
-       return (
-           <StatusAdvarselWrapper>
-               <Heading spacing size="small" level="3">
-                   Du har reservert deg mot digital kommunikasjon
-               </Heading>
-               Du kan ikke sende meldinger i den digitale dialogen fordi
-               du har reservert deg mot digital kommunikasjon i kontakt og reservasjonsregisteret (KRR).
-               <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
-                   Gå til Norge.no for å fjerne reservasjonen
-               </Link>
-           </StatusAdvarselWrapper>
-       );
-   }
-}
