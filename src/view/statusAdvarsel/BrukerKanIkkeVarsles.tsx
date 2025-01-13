fae61a60894b456ba76826579860bad8f296abc4
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-13 10:12:52 +0100

    riktig warning tekst om bruker ikke kan varsles

---
+++ src/view/statusAdvarsel/BrukerKanIkkeVarsles.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import StatusAdvarselWrapper from './StatusAdvarselWrapper';
+import { Heading, Link } from '@navikt/ds-react';
+
+const BrukerKanIkkeVarsles = () => (
+    <StatusAdvarselWrapper>
+            <Heading level="2" size="small">
+            Kontaktinformasjonen din er utdatert
+            </Heading>
+            Du kan ikke sende meldinger i dialogen fordi kontaktinformasjonen din er utdatert i kontakt og reservasjonsregisteret (KRR).
+            <br />
+            <Link href={'https://www.norge.no/nb/digital-borgar/registrere'}>
+                    Gå til norge.no for å oppdatere.
+            </Link>
+    </StatusAdvarselWrapper>
+);
+
+export default BrukerKanIkkeVarsles;
--- src/view/statusAdvarsel/StatusAdvarselWrapper.tsx
+++ src/view/statusAdvarsel/StatusAdvarselWrapper.tsx
@@ -1,4 +1,4 @@
-import { Alert } from '@navikt/ds-react';
+import { Alert, Heading, Link } from '@navikt/ds-react';
 import React from 'react';
 
 interface Props {
@@ -14,5 +14,15 @@ export default function StatusAdvarselWrapper({ children }: Props) {
 }
 
 export function KanIkkeKontakteElektroniskVeileder() {
-    return <StatusAdvarselWrapper>Du kan ikke kontakte denne brukeren digitalt.</StatusAdvarselWrapper>;
+
+    return <StatusAdvarselWrapper>
+        <Heading level="2" size="small">
+            Kontaktinfo til bruker er utdatert i KRR
+        </Heading>
+        Du kan ikke sende meldinger i dialogen fordi kontaktinformasjonen til brukeren er utdatert i KRR.
+        <br />
+        <Link href={'https://www.norge.no/nb/digital-borgar/oppdatere'}>
+            Brukeren må gå til norge.no for å oppdatere.
+        </Link>
+    </StatusAdvarselWrapper>;
 }
