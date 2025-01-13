3a387cbe4919af8ad25f94f40ed52875cd16cb61
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-13 12:04:14 +0100

    riktig tekst på reservert krr

--- src/view/statusAdvarsel/ReservertKrr.tsx
+++ src/view/statusAdvarsel/ReservertKrr.tsx
@@ -8,19 +8,19 @@ interface Props {
 }
 
 export default function ReservertKrr(props: Props) {
-    return props.erVeileder ? <KanIkkeKontakteElektroniskVeileder /> : <BrukerKrr />;
+    return props.erVeileder ? <KanIkkeKontakteElektroniskVeileder /> : <BrukerReservertIKrr />;
 }
 
-function BrukerKrr() {
+function BrukerReservertIKrr() {
        return (
            <StatusAdvarselWrapper>
                <Heading spacing size="small" level="3">
-                   Vi har ikke din kontaktinformasjon
+                   Du har reservert deg mot digital kommunikasjon
                </Heading>
-               Du kan ikke sende meldinger i dialogen fordi du ikke har registrert
-               e-post eller telefonnummeret ditt i kontakt og reservasjonsregisteret (KRR).
+               Du kan ikke sende meldinger i den digitale dialogen fordi du har
+               reservert deg mot digital kommunikasjon i kontakt og reservasjonsregisteret (KRR).
                <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
-                   Gå til norge.no for å registrere.
+                   Gå til norge.no for å fjerne reservasjonen.
                </Link>
            </StatusAdvarselWrapper>
        )
