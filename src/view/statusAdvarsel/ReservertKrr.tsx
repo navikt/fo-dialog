4de62b1a8929e5911233949797607a97a206059b
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-15 13:27:50 +0100

    fikset tekst for veileder om bruker er reservert i krr

--- src/view/statusAdvarsel/ReservertKrr.tsx
+++ src/view/statusAdvarsel/ReservertKrr.tsx
@@ -1,17 +1,16 @@
 import { Link, Heading } from '@navikt/ds-react';
 import React from 'react';
-
-import StatusAdvarselWrapper, { KanIkkeKontakteElektroniskVeileder } from './StatusAdvarselWrapper';
+import StatusAdvarselWrapper from './StatusAdvarselWrapper';
 
 interface Props {
     erVeileder: boolean;
 }
 
 export default function ReservertKrr(props: Props) {
-    return props.erVeileder ? <KanIkkeKontakteElektroniskVeileder /> : <BrukerReservertIKrr />;
+    return props.erVeileder ? <BrukerReservertIKrr />:  <BrukerReservertIKrrVeileder />;
 }
 
-function BrukerReservertIKrr() {
+function BrukerReservertIKrrVeileder() {
     return (
         <StatusAdvarselWrapper>
             <Heading spacing size="small" level="3">
@@ -21,6 +20,22 @@ function BrukerReservertIKrr() {
             kontakt og reservasjonsregisteret (KRR).
             <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
                 Gå til norge.no for å fjerne reservasjonen.
+            </Link>
+        </StatusAdvarselWrapper>
+    );
+}
+
+function BrukerReservertIKrr() {
+    return (
+        <StatusAdvarselWrapper>
+            <Heading spacing size="small" level="3">
+                Brukeren er reservert i KRR
+            </Heading>
+            Du kan ikke sende meldinger fordi brukeren har
+            reservert seg mot digital kommunikasjon KRR.
+            <br />
+            <Link href="https://www.norge.no/nb/digital-borger/reservasjon">
+                Brukeren må gå til norge.no for å fjerne reservasjonen.
             </Link>
         </StatusAdvarselWrapper>
     );
