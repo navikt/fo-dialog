06d5956243488a21c735e0e614e6147b0023c06f
Henrik uran <henrik.uran.nykmark@nav.no>
2025-01-17 09:56:02 +0100

    legg til br for bedre lesbarhet

--- src/view/statusAdvarsel/ErIkkeRegistrertIKrrAdverselVeileder.tsx
+++ src/view/statusAdvarsel/ErIkkeRegistrertIKrrAdverselVeileder.tsx
@@ -15,6 +15,7 @@ function BrukerIkkeIIKrrVeileder() {
                 Brukeren er ikke registrert i KRR
             </Heading>
             Du kan ikke sende meldinger i dialogen fordi brukeren ikke har registrert e-post eller telefonnummeret sitt i KRR.
+            <br />
             <Link href={LinkIkkeRegistrertIKrr}>
                 Brukeren må gå til norge.no for å registrere.
             </Link>
